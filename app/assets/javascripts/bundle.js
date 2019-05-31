4de71193732484a8609ab32019cc60526b62812d
Elvis Yuan <elvisxb@gmail.com>
2019-05-31 09:01:18 -0400

    prepare for merge

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -810,7 +810,7 @@ function (_React$Component) {
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var _server_index_container__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./server_index_container */ "./frontend/components/mainapp/server_index_container.jsx");
+/* harmony import */ var _server_index_container__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./server_index_container */ "./frontend/components/mainapp/server_index_container.js");
 /* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! react-router-dom */ "./node_modules/react-router-dom/esm/react-router-dom.js");
 /* harmony import */ var _channel_index__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./channel_index */ "./frontend/components/mainapp/channel_index.jsx");
 /* harmony import */ var _util_route_util__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../../util/route_util */ "./frontend/util/route_util.jsx");
@@ -898,154 +898,14 @@ function (_React$Component) {
 
 /***/ }),
 
-/***/ "./frontend/components/mainapp/server_icon.jsx":
-/*!*****************************************************!*\
-  !*** ./frontend/components/mainapp/server_icon.jsx ***!
-  \*****************************************************/
-/*! exports provided: ServerIcon, default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ServerIcon", function() { return ServerIcon; });
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
-
-var ServerIcon = function ServerIcon(props) {
-  var server = props.server;
-  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h3", {
-    className: "server-icon-text"
-  }, server.title.slice(0, 1).toLowerCase()), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, server.title));
-};
-/* harmony default export */ __webpack_exports__["default"] = (ServerIcon);
-
-/***/ }),
-
-/***/ "./frontend/components/mainapp/server_index.jsx":
-/*!******************************************************!*\
-  !*** ./frontend/components/mainapp/server_index.jsx ***!
-  \******************************************************/
+/***/ "./frontend/components/mainapp/server_index_container.js":
+/*!***************************************************************!*\
+  !*** ./frontend/components/mainapp/server_index_container.js ***!
+  \***************************************************************/
 /*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/***/ (function(module, exports) {
 
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-router-dom */ "./node_modules/react-router-dom/esm/react-router-dom.js");
-/* harmony import */ var _server_icon__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./server_icon */ "./frontend/components/mainapp/server_icon.jsx");
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-
-
-
-
-var ServerIndex =
-/*#__PURE__*/
-function (_React$Component) {
-  _inherits(ServerIndex, _React$Component);
-
-  function ServerIndex(props) {
-    _classCallCheck(this, ServerIndex);
-
-    return _possibleConstructorReturn(this, _getPrototypeOf(ServerIndex).call(this, props));
-  } // componentDidMount() {
-  //   const { currentUser, fetchAllServers } = this.props;
-  //   fetchAllServers(currentUser.id);
-  // }
-
-
-  _createClass(ServerIndex, [{
-    key: "render",
-    value: function render() {
-      var _this = this;
-
-      var servers = this.props.servers;
-      var serverList = servers ? servers.map(function (server, index) {
-        return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
-          onClick: function onClick() {
-            return _this.props.history.push("/servers/".concat(server.id));
-          },
-          key: index,
-          className: "button-flex server-btn blue-btn"
-        }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h3", {
-          className: "server-icon-text"
-        }, server.title.slice(0, 1).toLowerCase()), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, server.title));
-      }) : null;
-      return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-        className: "server-index-container"
-      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("ul", {
-        className: "server-ul"
-      }, serverList, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
-        className: "server-btn"
-      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h3", {
-        className: "server-icon"
-      }, "+"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "Add a Server"))));
-    }
-  }]);
-
-  return ServerIndex;
-}(react__WEBPACK_IMPORTED_MODULE_0___default.a.Component);
-
-/* harmony default export */ __webpack_exports__["default"] = (ServerIndex);
-
-/***/ }),
-
-/***/ "./frontend/components/mainapp/server_index_container.jsx":
-/*!****************************************************************!*\
-  !*** ./frontend/components/mainapp/server_index_container.jsx ***!
-  \****************************************************************/
-/*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var react_redux__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react-redux */ "./node_modules/react-redux/es/index.js");
-/* harmony import */ var _actions_server_actions__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../actions/server_actions */ "./frontend/actions/server_actions.js");
-/* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! react-router-dom */ "./node_modules/react-router-dom/esm/react-router-dom.js");
-/* harmony import */ var _server_index__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./server_index */ "./frontend/components/mainapp/server_index.jsx");
-
-
-
-
-
-var msp = function msp(_ref) {
-  var entities = _ref.entities,
-      session = _ref.session;
-  return {
-    currentUser: session.currentUser,
-    servers: Object.values(entities.servers)
-  };
-};
-
-var mdp = function mdp(dispatch) {
-  return {
-    fetchAllServers: function fetchAllServers(userId) {
-      return dispatch(Object(_actions_server_actions__WEBPACK_IMPORTED_MODULE_1__["fetchAllServers"])(userId));
-    },
-    fetchServer: function fetchServer(id) {
-      return dispatch(Object(_actions_server_actions__WEBPACK_IMPORTED_MODULE_1__["fetchServer"])(id));
-    }
-  };
-};
-
-/* harmony default export */ __webpack_exports__["default"] = (Object(react_router_dom__WEBPACK_IMPORTED_MODULE_2__["withRouter"])(Object(react_redux__WEBPACK_IMPORTED_MODULE_0__["connect"])(msp, mdp)(_server_index__WEBPACK_IMPORTED_MODULE_3__["default"])));
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nError: ENOENT: no such file or directory, open '/Users/Elvis/Desktop/Disco-RD/frontend/components/mainapp/server_index_container.js'");
 
 /***/ }),
 
