f53a84bc712a85383f46d9f8105f2363ffc2d87a
Elvis Yuan <elvisxb@gmail.com>
2019-06-25 17:15:32 -0400

    style video chat

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -2933,6 +2933,9 @@ function (_React$Component) {
     key: "componentDidUpdate",
     value: function componentDidUpdate(prevProps) {
       if (prevProps.match.params.channelId !== this.props.match.params.channelId) {
+        if (this.state.video === true) this.setState({
+          video: false
+        });
         this.props.fetchMessages(this.props.match.params.channelId);
         App[prevProps.match.params.channelId].unsubscribe();
         this.currentChannelId = this.props.match.params.channelId;
@@ -3035,10 +3038,11 @@ function (_React$Component) {
       return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         className: "chat-component-container"
       }, this.state.video ? react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_video__WEBPACK_IMPORTED_MODULE_6__["default"], {
+        username: username,
         user_id: currentUserId,
         toggleVideo: this.startVoice,
         currentChannelId: this.props.match.params.channelId
-      }) : null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+      }) : null, this.state.video ? null : react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         className: "channel-heading-wrapper"
       }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         className: "channel-heading-channel-title"
@@ -3060,10 +3064,25 @@ function (_React$Component) {
         d: "M12 2C6.486 2 2 6.486 2 12C2 17.515 6.486 22 12 22C14.039 22 15.993 21.398 17.652 20.259L16.521 18.611C15.195 19.519 13.633 20 12 20C7.589 20 4 16.411 4 12C4 7.589 7.589 4 12 4C16.411 4 20 7.589 20 12V12.782C20 14.17 19.402 15 18.4 15L18.398 15.018C18.338 15.005 18.273 15 18.209 15H18C17.437 15 16.6 14.182 16.6 13.631V12C16.6 9.464 14.537 7.4 12 7.4C9.463 7.4 7.4 9.463 7.4 12C7.4 14.537 9.463 16.6 12 16.6C13.234 16.6 14.35 16.106 15.177 15.313C15.826 16.269 16.93 17 18 17L18.002 16.981C18.064 16.994 18.129 17 18.195 17H18.4C20.552 17 22 15.306 22 12.782V12C22 6.486 17.514 2 12 2ZM12 14.599C10.566 14.599 9.4 13.433 9.4 11.999C9.4 10.565 10.566 9.399 12 9.399C13.434 9.399 14.6 10.565 14.6 11.999C14.6 13.433 13.434 14.599 12 14.599Z"
       }))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h2", {
         className: "channel-header-channel-title"
-      }, username)), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+      }, username)), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "call-icon-container"
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         className: "video-icon",
         onClick: this.startVoice
-      }, "Video Call")))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("svg", {
+        x: "0",
+        y: "0",
+        name: "Nova_CallVideo",
+        className: "video-icon-svg",
+        width: "24",
+        height: "24",
+        viewBox: "0 0 24 24"
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("path", {
+        fill: "currentColor",
+        d: "M21.526 8.149C21.231 7.966 20.862 7.951 20.553 8.105L18 9.382V7C18 5.897 17.103 5 16 5H4C2.897 5 2 5.897 2 7V17C2 18.104 2.897 19 4 19H16C17.103 19 18 18.104 18 17V14.618L20.553 15.894C20.694 15.965 20.847 16 21 16C21.183 16 21.365 15.949 21.526 15.851C21.82 15.668 22 15.347 22 15V9C22 8.653 21.82 8.332 21.526 8.149Z"
+      })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+        className: "direct-hovers"
+      }, "Start Video Call"))))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         className: "chatroom-container"
       }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         className: "chat-box-component"
@@ -3639,6 +3658,7 @@ function (_React$Component) {
       })["catch"](function (error) {
         console.log(error);
       });
+      this.joinCall();
     }
   }, {
     key: "componentWillUnmount",
@@ -3829,17 +3849,43 @@ function (_React$Component) {
       return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         className: "video-chat-component"
       }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "channel-heading-wrapper background-black"
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "channel-heading-channel-title"
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "channel-icon-wrapper"
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "direct-message-at"
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("svg", {
+        x: "0",
+        y: "0",
+        name: "Nova_At",
+        className: "at-symbol",
+        "aria-hidden": "false",
+        width: "24",
+        height: "24",
+        viewBox: "0 0 24 24"
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("path", {
+        fill: "currentColor",
+        d: "M12 2C6.486 2 2 6.486 2 12C2 17.515 6.486 22 12 22C14.039 22 15.993 21.398 17.652 20.259L16.521 18.611C15.195 19.519 13.633 20 12 20C7.589 20 4 16.411 4 12C4 7.589 7.589 4 12 4C16.411 4 20 7.589 20 12V12.782C20 14.17 19.402 15 18.4 15L18.398 15.018C18.338 15.005 18.273 15 18.209 15H18C17.437 15 16.6 14.182 16.6 13.631V12C16.6 9.464 14.537 7.4 12 7.4C9.463 7.4 7.4 9.463 7.4 12C7.4 14.537 9.463 16.6 12 16.6C13.234 16.6 14.35 16.106 15.177 15.313C15.826 16.269 16.93 17 18 17L18.002 16.981C18.064 16.994 18.129 17 18.195 17H18.4C20.552 17 22 15.306 22 12.782V12C22 6.486 17.514 2 12 2ZM12 14.599C10.566 14.599 9.4 13.433 9.4 11.999C9.4 10.565 10.566 9.399 12 9.399C13.434 9.399 14.6 10.565 14.6 11.999C14.6 13.433 13.434 14.599 12 14.599Z"
+      }))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h2", {
+        className: "channel-header-channel-title"
+      }, this.props.username)))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         className: "video-chat-container"
       }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         id: "remote-video-container"
       }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("video", {
         id: "local-video",
         autoPlay: true
-      })), this.state.joined ? react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
+      })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "video-button-container"
+      }, this.state.joined ? react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
+        className: "leave-call-button",
         onClick: this.leaveCall
       }, "Leave Call") : react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
+        className: "join-call-button",
         onClick: this.joinCall
-      }, "Join Call"));
+      }, "Join Call")));
     }
   }]);
 
--- frontend/components/mainapp/direct_message/direct_message.jsx
+++ frontend/components/mainapp/direct_message/direct_message.jsx
@@ -24,6 +24,7 @@ class DirectMessage extends React.Component {
     if (
       prevProps.match.params.channelId !== this.props.match.params.channelId
     ) {
+      if (this.state.video === true) this.setState({ video: false });
       this.props.fetchMessages(this.props.match.params.channelId);
       App[prevProps.match.params.channelId].unsubscribe();
 
@@ -136,40 +137,57 @@ class DirectMessage extends React.Component {
       <div className="chat-component-container">
         {this.state.video ? (
           <Video
+            username={username}
             user_id={currentUserId}
             toggleVideo={this.startVoice}
             currentChannelId={this.props.match.params.channelId}
           />
         ) : null}
-        <div className="channel-heading-wrapper">
-          <div className="channel-heading-channel-title">
-            <div className="channel-icon-wrapper">
-              <div className="direct-message-at">
-                <svg
-                  x="0"
-                  y="0"
-                  name="Nova_At"
-                  className="at-symbol"
-                  aria-hidden="false"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M12 2C6.486 2 2 6.486 2 12C2 17.515 6.486 22 12 22C14.039 22 15.993 21.398 17.652 20.259L16.521 18.611C15.195 19.519 13.633 20 12 20C7.589 20 4 16.411 4 12C4 7.589 7.589 4 12 4C16.411 4 20 7.589 20 12V12.782C20 14.17 19.402 15 18.4 15L18.398 15.018C18.338 15.005 18.273 15 18.209 15H18C17.437 15 16.6 14.182 16.6 13.631V12C16.6 9.464 14.537 7.4 12 7.4C9.463 7.4 7.4 9.463 7.4 12C7.4 14.537 9.463 16.6 12 16.6C13.234 16.6 14.35 16.106 15.177 15.313C15.826 16.269 16.93 17 18 17L18.002 16.981C18.064 16.994 18.129 17 18.195 17H18.4C20.552 17 22 15.306 22 12.782V12C22 6.486 17.514 2 12 2ZM12 14.599C10.566 14.599 9.4 13.433 9.4 11.999C9.4 10.565 10.566 9.399 12 9.399C13.434 9.399 14.6 10.565 14.6 11.999C14.6 13.433 13.434 14.599 12 14.599Z"
-                  />
-                </svg>
+        {this.state.video ? null : (
+          <div className="channel-heading-wrapper">
+            <div className="channel-heading-channel-title">
+              <div className="channel-icon-wrapper">
+                <div className="direct-message-at">
+                  <svg
+                    x="0"
+                    y="0"
+                    name="Nova_At"
+                    className="at-symbol"
+                    aria-hidden="false"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M12 2C6.486 2 2 6.486 2 12C2 17.515 6.486 22 12 22C14.039 22 15.993 21.398 17.652 20.259L16.521 18.611C15.195 19.519 13.633 20 12 20C7.589 20 4 16.411 4 12C4 7.589 7.589 4 12 4C16.411 4 20 7.589 20 12V12.782C20 14.17 19.402 15 18.4 15L18.398 15.018C18.338 15.005 18.273 15 18.209 15H18C17.437 15 16.6 14.182 16.6 13.631V12C16.6 9.464 14.537 7.4 12 7.4C9.463 7.4 7.4 9.463 7.4 12C7.4 14.537 9.463 16.6 12 16.6C13.234 16.6 14.35 16.106 15.177 15.313C15.826 16.269 16.93 17 18 17L18.002 16.981C18.064 16.994 18.129 17 18.195 17H18.4C20.552 17 22 15.306 22 12.782V12C22 6.486 17.514 2 12 2ZM12 14.599C10.566 14.599 9.4 13.433 9.4 11.999C9.4 10.565 10.566 9.399 12 9.399C13.434 9.399 14.6 10.565 14.6 11.999C14.6 13.433 13.434 14.599 12 14.599Z"
+                    />
+                  </svg>
+                </div>
+                <h2 className="channel-header-channel-title">{username}</h2>
               </div>
-              <h2 className="channel-header-channel-title">{username}</h2>
-            </div>
-            <div>
-              <div className="video-icon" onClick={this.startVoice}>
-                Video Call
+              <div className="call-icon-container">
+                <div className="video-icon" onClick={this.startVoice}>
+                  <svg
+                    x="0"
+                    y="0"
+                    name="Nova_CallVideo"
+                    className="video-icon-svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M21.526 8.149C21.231 7.966 20.862 7.951 20.553 8.105L18 9.382V7C18 5.897 17.103 5 16 5H4C2.897 5 2 5.897 2 7V17C2 18.104 2.897 19 4 19H16C17.103 19 18 18.104 18 17V14.618L20.553 15.894C20.694 15.965 20.847 16 21 16C21.183 16 21.365 15.949 21.526 15.851C21.82 15.668 22 15.347 22 15V9C22 8.653 21.82 8.332 21.526 8.149Z"
+                    />
+                  </svg>
+                  <p className="direct-hovers">Start Video Call</p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        )}
         <div className="chatroom-container">
           <div className="chat-box-component">
             <div className="message-window">
