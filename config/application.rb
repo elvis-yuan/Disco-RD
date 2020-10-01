fe10f457c7bb72ac7a2aac4a7622aa0a7651a01b
Elvis Yuan <elvisxb@gmail.com>
2020-10-01 13:21:49 -0400

    fix file locations

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module DiscoRd
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
