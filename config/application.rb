c73527a91a480c704878be0a8bca04d9a0c07c95
Elvis Yuan <elvisxb@gmail.com>
2020-10-01 14:00:37 -0400

    add scss

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ module DiscoRd
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
-    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
